b947ac6a86788eae3c4f81badb56913258ec5305
ElieThellier <eliethellier@gmail.com>
2022-06-17 11:14:23 +0200

    chaine

---
+++ afficher_carac.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Entrez la chaîne de caractère à afficher :\n");
+    char mot[50];
+    scanf("%[^\n]",mot);
+    printf("%s\n",mot);
+    return 0;
+}
