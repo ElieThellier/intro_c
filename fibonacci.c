14f14cae8fc62574c7bf5d27b7170c917522320a
ElieThellier <eliethellier@gmail.com>
2022-06-17 10:10:55 +0200

    m

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+int fibo(int n){
+    int F[n];
+    F[0] = 0;
+    F[1] = 1;
+    for (int i=2;i<=n;i=i+1){
+        F[i]=F[i-2]+F[i-1];
+    }
+    return F[n];
+}
 
-
-
-int main(){
+int main(int n){
+    printf("Entrez l'indice du nombre de la suite de Fibonacci voulu :");
+    scanf("%d",&n);
+    printf("Le %d-ème nombre de la suite de Fibonacci est %d\n",n , fibo(n));
     return 0;
 }
 
---
+++ pairs100.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_tableau(int tab[],int len){
+    for (int i=0; i<len; i++){
+        printf("%d\n", tab[i]);
+    }
+}
+
+int main(int n){
+    printf("Afficher les entiers pairs jusqu'à ?");
+    scanf("%d",&n);
+    n=n/2;
+    int tab[n];
+    int j=0;
+    for (int i=0;i<=2*n;i=i+2){
+        tab[j]=i;
+        j=j+1;
+    }
+    print_tableau(tab,n+1);
+    return 0;
+}
